96479f05cadf60ad3508b39b6fda7bd668fe02a6
Vasyl Kostyrko <crocodile21@gmail.com>
2022-09-21 21:20:31 +0300

    first

--- kw_library/__manifest__.py
+++ kw_library/__manifest__.py
@@ -3,15 +3,15 @@
     'name': "Library",
     'summary': "",
 
-    'author': "Kitworks Systems",
-    'website': "https://kitworks.systems",
+    'author': "Василь Костирко",
+    'website': "https://letu.lviv.ua",
 
-    'category': 'Customization',
+    'category': 'Education',
     'license': 'OPL-1',
     'version': '15.0.1.0.0',
 
     'depends': [],
-    'external_dependencies': {'python':[],},
+    'external_dependencies': {'python': [],},
 
     'data': ['security/ir.model.access.csv',
              'views/menu.xml',
