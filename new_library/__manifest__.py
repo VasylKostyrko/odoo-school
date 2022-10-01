39019f4bb55d340331b21ec7d2baf526e64e4640
vasyl kostyrko <crocodile212gmail.com>
2022-10-01 18:33:47 +0300

    Права доступу

---
+++ new_library/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "New Library",
+    "description": """Learning Odoo Qweb.""",
+    "summary": "Learning Odoo Qweb",
+    "author": "kbc",
+    "category": "Education",
+    "version": "15.0.1.0.0",
+    "depends": [
+        "web", "point_of_sale",
+    ],
+    "qweb": [],
+    "data": [
+        "security/ir.model.access.csv",
+        "data/data.xml",
+        "views/models_views.xml",
+    ],
+}
