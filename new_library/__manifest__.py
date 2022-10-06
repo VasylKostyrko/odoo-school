97f203ca044b8adf79479e395b6b8f2bfceb62e2
vasyl kostyrko <crocodile212gmail.com>
2022-10-06 20:47:29 +0300

    Виправлена new_library 5

--- new_library/__manifest__.py
+++ new_library/__manifest__.py
@@ -1,17 +1,28 @@
 {
     "name": "New Library",
     "summary": "Learning Odoo Qweb",
+
     "author": "kbc",
     "category": "Education",
     "version": "15.0.1.0.0",
     "license": "OPL-1",
     "depends": [
-        "web", "point_of_sale",
+        # "web", "point_of_sale",
     ],
+    'external_dependencies': {'python': [], },
     "qweb": [],
     "data": [
         "security/ir.model.access.csv",
-        "data/data.xml",
-        "views/models_views.xml",
+        'views/menu.xml',
+        'views/book.xml',
+        'views/author.xml',
     ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'images': [
+               'static/description/icon.png',
+               ],
+    'price': 0,
+    'currency': 'EUR',
 }
