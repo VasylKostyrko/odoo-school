6865fa3f4631afcfc3d694e94219909185081e11
vasyl kostyrko <crocodile212gmail.com>
2022-10-09 11:56:22 +0300

    Виправлена new_library 6

--- new_library/models/book.py
+++ new_library/models/book.py
@@ -5,10 +5,10 @@ _logger = logging.getLogger(__name__)
 
 class Book(models.Model):
     _name = 'new.lib.book'
-    _description = 'Library Book'
+    _description = 'Library_Book'
     name = fields.Char()
-    active = fields.Boolean(default=True,)
     isbn = fields.Char()
-    additional_text = fields.Text()
+    book_description= fields.Text()
+    active = fields.Boolean(default=True,)
     author_ids = fields.Many2many(
         comodel_name='new.lib.author', )
