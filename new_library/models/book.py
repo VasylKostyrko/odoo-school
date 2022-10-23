2fa7a3f8f25b96fb49ccf5437489c7f95dc2d66b
Vasyl Kostyrko <crocodile21@gmail.com>
2022-10-23 11:52:59 +0300

    VMware Ubuntu 2 - 2

--- new_library/models/book.py
+++ new_library/models/book.py
@@ -9,7 +9,8 @@ class Book(models.Model):
     name = fields.Char()
     isbn = fields.Char()
     lib_num = fields.Char()
-    num_item = fields.Integer()
+    num_item = fields.Integer(default=1,)
+    num_available = fields.Integer(default=1,)
     description = fields.Text()
     active = fields.Boolean(default=True,)
     author_ids = fields.Many2many(
